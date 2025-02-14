cfd5feecd8f97031cfea6f6640a716885d7fec08
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-02-14 14:12:38 +0300

    First commit: Create UI

--- Quiz/ViewControllers/QuestionsViewController.swift
+++ Quiz/ViewControllers/QuestionsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class QuestionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,5 @@ class ViewController: UIViewController {
 
 
 }
+
 

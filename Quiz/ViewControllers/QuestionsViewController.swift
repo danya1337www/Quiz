193ea2d2075d2d1330f0bbaac1c3ee4f4039bbf2
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-02-23 15:58:16 +0300

    Update

--- Quiz/ViewControllers/QuestionsViewController.swift
+++ Quiz/ViewControllers/QuestionsViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 final class QuestionsViewController: UIViewController {
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    
+    private let questions = Question.getQuestions()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
